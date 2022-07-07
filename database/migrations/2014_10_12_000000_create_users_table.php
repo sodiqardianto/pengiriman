d207dcd8ce0e849b8241bb3f7c27f28cb25d036e
sodiqardianto <sodiqardianto@gmail.com>
2022-07-07 18:21:21 +0700

    update table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->bigInteger('no_telp');
+            $table->string('jabatan');
+            $table->string('alamat');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
