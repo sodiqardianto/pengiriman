124d896048c6e382c5eb9557b63cf00a2e45ec1b
sodiqardianto <sodiqardianto@gmail.com>
2024-01-07 17:34:45 +0700

    update composer.lock dan migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->bigInteger('no_telp');
+            $table->string('no_telp');
             $table->string('jabatan');
             $table->string('alamat');
             $table->timestamp('email_verified_at')->nullable();
